69376d69ace0628beaeded5eb7dd83e3f7463da3
Lee Ayala <P2655286@my365.dmu.ac.uk>
2023-02-25 18:05:50 +0000

    just testing out how to commit and push branch

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAddress anAddress = new clsAddress();
+            Assert.IsNotNull(anAddress);
+        }
+    }
+}
